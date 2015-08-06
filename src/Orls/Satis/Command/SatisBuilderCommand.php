0b41c89188383093b30984365b42d73c66631594
Owen Smith <owen@duedil.com>
2015-08-06 14:34:46 +0100

    Basic requirements of Symfony console

--- src/Orls/Satis/Command/SatisBuilderCommand.php
+++ src/Orls/Satis/Command/SatisBuilderCommand.php
@@ -2,6 +2,27 @@
 
 namespace Orls\Satis\Command;
 
-class SatisBuilderCommand
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SatisBuilderCommand extends Command
 {
+    protected function configure()
+    {
+        $this->setName('build');
+        $this->addArgument(
+            'sources',
+            InputArgument::IS_ARRAY,
+            'The composer.json file(s) to build the satis conf for (separate' .
+            'multiple files with spaces)'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("<error>Not yet implemented</error>");
+    }
 }
