e2e35bf6560bbc871127e271f255c37c3fc56743
Owen Smith <owen@duedil.com>
2015-08-07 17:24:23 +0100

    Main command implementation:
    - take a template file
    - gather reqs and repos from all composer json files given
    - put them into the template and echo

--- src/Orls/Satis/Command/SatisBuilderCommand.php
+++ src/Orls/Satis/Command/SatisBuilderCommand.php
@@ -8,11 +8,21 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Composer\Json\JsonFile;
+
+use Orls\Satis\RequirementsMerger;
+use Orls\Satis\RepositoriesMerger;
+
 class SatisBuilderCommand extends Command
 {
     protected function configure()
     {
         $this->setName('build');
+        $this->addArgument(
+            'satisTemplate',
+            InputArgument::REQUIRED,
+            'The skeleton satis.json file(s) to build'
+        );
         $this->addArgument(
             'composerJson',
             InputArgument::IS_ARRAY | InputArgument::REQUIRED,
@@ -23,6 +33,31 @@ class SatisBuilderCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("<error>Not yet implemented</error>");
+        $satisTemplatePath = $input->getArgument('satisTemplate');
+        $sourcePaths = $input->getArgument('composerJson');
+
+        $templateJson = new JsonFile($satisTemplatePath);
+        $satisFile = $templateJson->read();
+
+        $sources = array();
+        foreach ($sourcePaths as $sourcePath) {
+            $jsonFile = new JsonFile($sourcePath);
+            $sources []= $jsonFile->read();
+        }
+
+        $reqsMerger = new RequirementsMerger();
+        $reposMerger = new RepositoriesMerger();
+
+        $reqs = $reqsMerger->getRequirements($sources);
+        $repos = $reposMerger->getRepositories($sources);
+
+        // always append packagist
+        $repos []= array("type" => "composer","url" => "http://packagist.org");
+
+        $satisFile['require-all'] = false;
+        $satisFile['require'] = $reqs;
+        $satisFile['repositories'] = $repos;
+
+        $output->write(JsonFile::encode($satisFile));
     }
 }
